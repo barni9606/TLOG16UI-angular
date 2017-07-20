e4a42e0a264b83e528242365f2fe3aacd02bbc7c
bschenk <bschenk@precognox.com>
2017-07-20 15:51:38 +0200

    pagination works

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -13,6 +13,14 @@ export class CalendarComponent {
   dayNames: String[] = ['Mon', "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   dateSubscription: Subscription;
   private date: Date;
+  private onlyDayChanging : boolean = false;
+  //for drawing the calendar
+  firstDay: number;
+  daysInMonth : number;
+  leftOfWeek : number;
+  numberOfWeeks : number;
+  weeks: number[];
+
 
   selectDay(day:number): void {
 
@@ -21,7 +29,26 @@ export class CalendarComponent {
 
   constructor(private dateService: DateService) {}
   ngOnInit(): void {
-    this.dateSubscription = this.dateService.dateObservable.subscribe(date => this.date = date);
+    this.dateSubscription = this.dateService.dateObservable.subscribe(date => {
+      this.date = date;
+
+      //for drawing
+      this.leftOfWeek = 7;
+      this.firstDay = this.dateService.getFirstDayOfWeekInMonth();
+      if(this.firstDay === 0){
+        this.firstDay = 7;
+      }
+      this.firstDay = this.firstDay * -1 + 2;
+      this.daysInMonth = this.dateService.getNumberOfDaysInMonth();
+      this.numberOfWeeks = -1 * this.firstDay + this.daysInMonth + 1;
+      this.numberOfWeeks = Math.ceil(this.numberOfWeeks / 7);
+      this.weeks = Array(this.numberOfWeeks);
+      let asd: number = this.firstDay;
+      for(let i = 0; i < this.weeks.length; i++){
+        this.weeks[i] = asd;
+        asd += 7;
+      }
+    });
   }
   ngOnDestroy() {
     this.dateSubscription.unsubscribe();
--- src/app/week/week.component.ts
+++ src/app/week/week.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'week',
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
 
 })
 
-export class WeekComponent {
+export class WeekComponent{
+  @Input() firstDayOfWeek: number;
+  @Input() lastDayOfMonth: number;
+  days: number[];
+  ngOnInit():void{
+    if(this.lastDayOfMonth - this.firstDayOfWeek < 7){
+      this.days = Array(this.lastDayOfMonth - this.firstDayOfWeek + 1);
 
+    } else {
+
+      this.days = Array(7);
+    }
+    for(let i = 0; i < this.days.length; i++){
+      this.days[i] = this.firstDayOfWeek + i;
+    }
+  }
 }
